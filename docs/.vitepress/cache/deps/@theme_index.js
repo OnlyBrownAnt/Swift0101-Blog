ba75725283615047a880940a1fe453159936162f
zhangyao <45zhangye@gmail.com>
2023-03-04 22:40:01 +0800

    新增文档

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,24 +1,24 @@
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import NotFound from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
-import { default as default2 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/yaozhang/Desktop/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import NotFound from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
+import { default as default2 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "/Users/yaozhang/Desktop/Swift0101-Blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   NotFound,
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,6 @@
 # Swift0101 Blog
 ## hello {#hello}
 [Getting Started](/guide/getting-started)
+
+## markdown {#markdown}
+[基础教程](/tools/markdown)
