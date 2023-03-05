5fe5ecd0204cf8db36ec6eac350f924f9536807a
zhangyao <45zhangye@gmail.com>
2023-03-05 23:06:29 +0800

    文档配置更新

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,13 @@
 export default {
-  lang: 'zh-Hans',
+  lang: 'zh',
   title: 'Swift0101 Blog',
   description: '',
   base: '/',
-  ignoreDeadLinks: true // TODO 打包和运行时忽略md文档报错: Found dead link 
+  lastUpdated: true,
+  ignoreDeadLinks: true, // TODO 打包和运行时忽略md文档报错: Found dead link 
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/OnlyBrownAnt' }
+    ]
+  }
 }
