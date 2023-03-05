8edaff48f52efc40407dddb2ea9157e1389221bb
zhangyao <45zhangye@gmail.com>
2023-03-06 02:01:52 +0800

    更新侧边栏和目录设置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,6 +8,87 @@ export default {
   themeConfig: {
     socialLinks: [
       { icon: 'github', link: 'https://github.com/OnlyBrownAnt' }
-    ]
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: '后端',
+          collapsed: false,
+          items: [
+            { text: 'Java 多线程总结', link: '/server/java/javaThreads'},
+            { text: 'Java Socket', link: '/server/java/JavaSocket'},
+            { text: '数据库-基础概念介绍', link: '/server/DB/dbBase'},
+            { text: 'Java 注解(Annotation)', link: '/server/java/JavaAnnotation'},
+            { text: 'Java 反射(reflect)', link: '/server/java/JavaReflect'},
+            { text: 'More', link: '/server/java/learnJava' },
+          ]
+        },
+        {
+          text: '前端',
+          collapsed: false,
+          items: [
+            { text: 'More', link: '/web/index' }
+          ]
+        },
+        {
+          text: '叮当猫的万能口袋',
+          collapsed: false,
+          items: [
+            { text: '你比你想象中还需要Markdown', link: '/tools/markdown/markdown' },
+            { text: 'SSH 实现免密登陆', link: '/tools/sshLoginWithoutPassword' },
+            { text: 'Postman Mock服务的基本使用教程', link: '/tools/postman/postmanMock' },
+            { text: 'Docker快速内网部署YApi Mock服务平台', link: '/tools/yapi/yapi-1' },
+            { text: 'JMeter的安装和简单使用', link: '/tools/jmeter/jmeterGuide' },
+            { text: 'More', link: '/tools/yapi/yapi-1' }
+          ]
+        }
+      ],
+      '/web/': [
+        {
+          text: '前端',
+          collapsed: false,
+          items: []
+        }
+      ],
+      '/server/': [
+        {
+          text: '后端',
+          collapsed: false,
+          items: [
+            { text: '学习JAVA', link: '/server/java/learnJava'},
+            { text: 'Java 多线程总结', link: '/server/java/javaThreads'},
+            { text: 'Java Socket', link: '/server/java/JavaSocket'},
+            { text: 'Java XML', link: '/server/java/JavaSocket'},
+            { text: 'Java JSON', link: '/server/java/JavaJSON'},
+            { text: 'Java 枚举(Enum)', link: '/server/java/JavaEnum'},
+            { text: 'Java 注解(Annotation)', link: '/server/java/JavaAnnotation'},
+            { text: 'Java 反射(reflect)', link: '/server/java/JavaReflect'},
+            { text: 'JAVA 多线程 转账Demo', link: '/server/java/javaThreadsTransferDemo'},
+            { text: '数据库-基础概念介绍', link: '/server/DB/dbBase'},
+            { text: 'MySQL Transactions', link: '/server/DB/MySqlTransactions'}
+          ]
+        }
+      ],
+      '/tools/': [
+        {
+          text: '叮当猫的万能口袋',
+          collapsed: false,
+          items: [
+            { text: 'Docker快速内网部署YApi Mock服务平台', link: '/tools/yapi/yapi-1' },
+            { text: 'Postman文档集', link: '/tools/postman/postmanMock' },
+            { text: 'Postman Mock服务的基本使用教程', link: '/tools/postman/postmanMock' },
+            { text: 'JMeter的安装和简单使用', link: '/tools/jmeter/jmeterGuide' },
+            { text: '你比你想象中还需要Markdown', link: '/tools/markdown/markdown' },
+            { text: '服务器的购买和基本使用教程', link: '/tools/others/orderServer' },
+            { text: 'IPad Tips 图书APP(iBook)', link: '/tools/others/IPadTips' },
+            { text: '你对甘特图最开始的印象是什么', link: '/tools/others/ganttChart' },
+            { text: '这是你想要的呼吸灯效果吗', link: '/tools/others/ESP32BreathingLights' },
+            { text: 'Git Operation timed out', link: '/tools/git/GitOperationTimedout' },
+            { text: 'IDEA 注释模版设置', link: '/tools/idea/ideaTemplateSetting' },
+            { text: 'SSH 实现免密登陆', link: '/tools/sshLoginWithoutPassword' }
+          ]
+        }
+      ]
+    }
   }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,37 +1,3 @@
 > My brain is overloaded
 
-## 最近 {#最近}
-[Docker快速内网部署YApi Mock服务平台](./tools/yapi/yapi-1)
-
-[Postman文档集](./tools/postman/postmanMock)
-
-[Postman Mock服务的基本使用教程](./tools/postman/postmanMock)
-
-[JMeter的安装和简单使用](./tools/jmeter/jmeterGuide)
-
-[你比你想象中还需要Markdown](./tools/markdown/markdown)
-
-## 前端 {#前端}
-[更多](./web/index)
-## 后端 {#后端}
-[Java 多线程总结](./server/java/javaThreads)
-
-[Java Socket](./server/java/JavaSocket)
-
-[更多](./server/index)
-## 叮当猫的口袋 {#叮当猫的口袋}
-[服务器的购买和基本使用教程](./tools/others/orderServer)
-
-[IPad Tips 图书APP(iBook)](./tools/others/IPadTips)
-
-[你对甘特图最开始的印象是什么](./tools/others/ganttChart)
-
-[这是你想要的呼吸灯效果吗](./tools/others/ESP32BreathingLights)
-
-[Git Operation timed out](./tools/git/GitOperationTimedout)
-
-[IDEA 注释模版设置](./tools/idea/ideaTemplateSetting)
-
-[SSH 实现免密登陆](./tools/sshLoginWithoutPassword)
-
-[更多](./tools/index)
+\>_< **人生不熄，Coding不止**
